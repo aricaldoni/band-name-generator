23dbc62e0091563320cd4167ae9db215a945bcfd
aricaldoni <>
2021-04-08 19:12:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Welcome message
+print("Welcome to the authomatic Band Name generator.")
+#Ask the user for the city that they grew up in
+city = input("What city did you grow up in?: \n")
+#Ask the user for the name of a pet
+pet = input("What is the name of your pet: \n")
+#Combine the name of their city and pet and show them their band name.
+print("The suggested name for your Band is "+ city + " " + pet)
